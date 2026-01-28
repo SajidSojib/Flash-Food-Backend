3da56d8d411abdbbf480b779f3fc0a4d6e7e9795
SajidSojib <sajidahmed2401@gmail.com>
2026-01-28 21:27:56 +0600

    service/added create and delete category

---
+++ src/modules/category/category.service.ts
@@ -0,0 +1,49 @@
+import type { Category } from "../../../generated/prisma/client"
+import { prisma } from "../../lib/prisma"
+import { ApiError } from "../../utils/apiError"
+
+const createCategory = async (data: Category) => {
+    data.slug = data.name.toLowerCase().replace(/ /g, "-");
+    const existingCategory = await prisma.category.findUnique({
+        where: {
+            slug: data.slug
+        }
+    })
+    if (existingCategory) {
+        throw new ApiError(400, "Category already exists")
+    }
+    
+    return await prisma.category.create({
+        data
+    })
+}
+
+const deleteCategory = async (id: string) => {
+    const category = await prisma.category.findUnique({
+        where: {
+            id
+        }
+    })
+    if(!category){
+        throw new ApiError(404, "Category not found")
+    }
+    if (!category) {
+        throw new ApiError(404, "Category not found")
+    }
+    await prisma.meal.deleteMany({
+        where: {
+            categoryId: id
+        }
+    })
+    return await prisma.category.delete({
+        where: {
+            id
+        }
+    })
+}
+
+
+export const categoryService = {
+    createCategory,
+    deleteCategory
+}
