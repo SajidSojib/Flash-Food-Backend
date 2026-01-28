ce941d16660e177be291cf82ca8edac322da795d
SajidSojib <sajidahmed2401@gmail.com>
2026-01-28 15:27:38 +0600

    middlewire/added not found route

---
+++ src/middlewire/notFound.ts
@@ -0,0 +1,13 @@
+import { type Request, type Response } from "express";
+
+const notFound = (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.originalUrl,
+    method: req.method,
+    date: Date(),
+  });
+};
+
+export default notFound;
